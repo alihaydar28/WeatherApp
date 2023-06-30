0f41665cde26b66d4ee00b041b9fef94b57e1211
ali haidar <alihaydar282828@gmail.com>
2023-07-01 00:51:44 +0300

    optimized js code

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 const searchBtn = document.querySelector('.searchBtn');
 const inputField = document.querySelector('.inputField');
-const cityName = document.querySelector('.cityName');
-const temp = document.querySelector('.temp');
-const feelslike = document.querySelector('.feelsLikeSpan');
-const humidity = document.querySelector('.humiditySpan');
-const wind = document.querySelector('.windSpan');
-
-searchBtn.addEventListener('click', function() {
-    // bdna n3ayet la shi function hon w naatiya l search result
-});
-
-async function getWeatherInfo(city ='beirut'){
-    try{
-        const response= await fetch(`https://api.weatherapi.com/v1/current.json?key=1298f3c1dfd04851afa122550233006&q=${city}`);
-        const weatherData = await response.json();
-
-        cityName.textContent=weatherData.location.name;
-        temp.textContent=weatherData.current.temp_c+" °C";
-        feelslike.textContent=weatherData.current.feelslike_c;
-        humidity.textContent=weatherData.current.humidity;
-        wind.textContent=(weatherData.current.wind_mph)*1.60;
-    }catch(err){
-        console.log(err);
-    }
+const weatherInfo = {
+  cityName: document.querySelector('.cityName'),
+  temp: document.querySelector('.temp'),
+  feelslike: document.querySelector('.feelsLikeSpan'),
+  humidity: document.querySelector('.humiditySpan'),
+  wind: document.querySelector('.windSpan')
 };
 
-getWeatherInfo('london');
+searchBtn.addEventListener('click', handleSearch);
+inputField.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    handleSearch();
+  }
+});
+
+async function getWeatherInfo(city = 'beirut') {
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1298f3c1dfd04851afa122550233006&q=${city}`);
+    const weatherData = await response.json();
+
+    const name=weatherData.location.name;
+    const { temp_c, feelslike_c, humidity, wind_mph } = weatherData.current;
+    updateWeatherInfo(name, temp_c, feelslike_c, humidity, wind_mph);
+
+  } catch (err) {
+    updateWeatherInfo("wrong city or field is empty", "-", "-", "-", "-");
+  }
+};
+
+function updateWeatherInfo(cityName, temp, feelslike, humidity, wind_mph) {
+  weatherInfo.cityName.textContent = cityName;
+  weatherInfo.temp.textContent = `${temp} °C`;
+  weatherInfo.feelslike.textContent = feelslike;
+  weatherInfo.humidity.textContent = humidity;
+  weatherInfo.wind.textContent = (wind_mph * 1.60).toFixed(2);
+}
+
+function handleSearch() {
+  getWeatherInfo(inputField.value);
+  inputField.value = "";
+}
+
+getWeatherInfo('beirut');
+
 
 
